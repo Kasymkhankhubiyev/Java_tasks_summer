4a5c651e25414f3e2f1980ec2a0537b82ca7f63e
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-07-25 02:25:16 +0700

    initial commit

--- src/main/java/task2/Calculator.java
+++ src/main/java/task2/Calculator.java
@@ -5,12 +5,25 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Map;
 import java.util.Stack;
+import java.io.File;
 
 public class Calculator {
     public static void main(String[] args) {
         CommandFactory commandFactory;
         Context context;
         InputStreamReader reader;
+
+        File myfile = new File("resources"); //Нужно нормальный путь к файлу записать.
+        if(myfile.exists()) {
+            System.out.println("File is found\n");
+            if(myfile.canRead()){
+
+            }
+        }
+        else {
+            System.out.println("File does not exist..." + '\n' + "input your commands using console\n");
+            //пишем для консольного ввода
+        }
     }
 
     private InputStreamReader readFormConsole(){

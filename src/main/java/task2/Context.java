f7bb7dcc4cb052ec76bfc3813613330bd2bf2e98
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-07-26 01:26:54 +0700

    Context commit

--- src/main/java/task2/Context.java
+++ src/main/java/task2/Context.java
@@ -1,11 +1,27 @@
 package task2;
 
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public interface Context {
-    Stack<Double> getStack();
-    Map<String, Double> getConstants();
-    Writer getOutput();
+public class Context {
+    Stack<Double> stack = new Stack<Double>();
+    Map<String, Double> map = new HashMap<String, Double>();
+
+    Context(Writer writer) {
+
+    }
+
+    Double getStack() { //дает самы верхний элемент и удаляется его из стека.
+        return stack.pop(); //почему не подходит?
+    }
+
+    Map<String, Double> getConstants() {
+        return null;
+    }
+
+    Writer getOutput() {
+        return null;
+    }
 }
--- src/main/java/task2/commands/Contextlmp.java
+++ src/main/java/task2/commands/Contextlmp.java
@@ -1,4 +1,28 @@
 package task2.commands;
 
-public class Contextlmp {
-}
+import task2.Context;
+
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Map;
+import java.util.Stack;
+import java.io.File;
+
+//public class Contextlmp extends Context {
+
+    //Contextlmp(Writer file) {
+       // Stack stack = new Stack();
+    //}
+    //public Stack<Double> getStack() {
+      //  return null;
+    //}
+//
+  //  public Map<String, Double> getConstants() {
+    //    return null;
+    //}
+//
+  //  public Writer getOutput() {
+    //    return null;
+    //}
+
+//}

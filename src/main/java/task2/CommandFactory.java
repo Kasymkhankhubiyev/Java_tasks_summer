1f7791276d15c38e67921614d8b3c5f662fc9049
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-07-26 10:42:58 +0700

    CommandFactory commit

--- src/main/java/task2/CommandFactory.java
+++ src/main/java/task2/CommandFactory.java
@@ -1,13 +1,29 @@
 package task2;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 
 public class CommandFactory {
     private Map<String, Command> commandMap = new HashMap<>();
+    static char terminator = ' '; //это верно?
+
 
     private void init(){
         getClass().getResourceAsStream("commands.txt");
+        try{
+            File file = new File("commands.txt");
+            FileInputStream filestream = null;
+            filestream = new FileInputStream("commands.txt");
+            Properties prop = new Properties();
+            prop.load(filestream);
+
+
+        }catch (Exception e){}
 
         //fill coommandMap with actual data
     };

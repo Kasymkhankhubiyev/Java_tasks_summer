ca812c4d0023cfe0aad5567b3b06a2a688ad2454
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-07-26 11:28:38 +0700

    Commands list commit

--- src/main/java/task2/Command.java
+++ src/main/java/task2/Command.java
@@ -5,3 +5,52 @@ import java.util.List;
 public interface Command {
     void execute(Context context, List<String> args) throws CommandException;
 }
+
+abstract class DEFINE implements Command{
+    @Override
+    public void execute (Context context, List<String> args){
+
+    }
+}
+
+abstract class POP implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}
+
+abstract class PUSH implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}
+
+abstract class SUM implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}
+
+abstract class DIF implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}
+
+abstract class MULTI implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}
+
+abstract class DIV implements Command{
+    @Override
+    public void execute(Context context, List<String> args){
+
+    }
+}

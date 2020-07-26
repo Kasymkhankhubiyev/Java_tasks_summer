b1b70f227b613b85a87220a707dfde9aa1c69ee6
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-07-26 11:08:42 +0700

    Command reader commit

--- src/main/java/task2/CommandFactory.java
+++ src/main/java/task2/CommandFactory.java
@@ -1,17 +1,15 @@
 package task2;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.File;
-import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.Scanner;
 
 
 public class CommandFactory {
     private Map<String, Command> commandMap = new HashMap<>();
-    static char terminator = ' '; //это верно?
-
+    String terminator = " "; //это верно?
 
     private void init(){
         getClass().getResourceAsStream("commands.txt");
@@ -19,11 +17,24 @@ public class CommandFactory {
             File file = new File("commands.txt");
             FileInputStream filestream = null;
             filestream = new FileInputStream("commands.txt");
-            Properties prop = new Properties();
-            prop.load(filestream);
+            Scanner reader = null;
+            reader = new Scanner(filestream);
+            if(reader!=null){
+                while(reader.hasNext()){
+                    String string = reader.nextLine();
+                    String[] substring;
+                    substring = string.split(terminator,2);// я теперь должен вытаскивать значения? зачем вообще double в мапке?
+                    commandMap.put(substring[0],substring[1]);
+                }
+            } else throw new IOException();
+            //Properties prop = new Properties(); // создаю контейнер и откгружаю все сюда.
+            //prop.load(filestream);
 
-
-        }catch (Exception e){}
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
         //fill coommandMap with actual data
     };

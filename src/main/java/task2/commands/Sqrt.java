5c2b250daf2cfdc713d52609f80a903411837d2b
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-08-02 13:46:35 +0700

    Sqrt commit

--- src/main/java/task2/commands/Sqrt.java
+++ src/main/java/task2/commands/Sqrt.java
@@ -9,6 +9,24 @@ import java.util.List;
 public class Sqrt implements Command {
     @Override
     public void execute(Context context, List<String> args) throws CommandException {
+        try{
+            final double check = 0.0000000001;
+            String variableName1 = args.get(0);
 
+            double variable1 = context.getConstants().remove(variableName1);
+
+            double a = context.getStack().pop();//два самых верхних из стека
+
+            if((Math.abs(a - variable1)<check)){
+                if(variable1>0){
+                    context.getConstants().put(variableName1, Math.sqrt(variable1));
+                    context.getStack().push(variable1);
+                }else
+                    throw new CommandException("Variable is negative, sqrt can't be done");
+            }else
+                throw new CommandException("Variable must be the highest in the Stack");
+        }catch(Exception e){
+            throw new CommandException("Variables not found");
+        }
     }
 }

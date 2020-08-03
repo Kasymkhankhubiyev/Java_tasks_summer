23612d944ef87275e5d3af4f5ec7678b984dbd17
DmitryNuzhdin <mr.dmitrynuzhdin@yandex.ru>
2020-08-03 14:10:59 +0300

    Task3

---
+++ src/main/java/task3/model/Coordinate.java
@@ -0,0 +1,35 @@
+package task3.model;
+
+import java.util.Objects;
+
+public class Coordinate {
+    public final int x;
+    public final int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordinate rotate(Coordinate point) {
+        return null;
+    }
+
+    public Coordinate move(int dx, int dy) {
+        return new Coordinate(x + dx, y + dy);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}

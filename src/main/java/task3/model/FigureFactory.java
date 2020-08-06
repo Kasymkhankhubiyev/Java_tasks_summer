5bbb935c389d451ebbbca6cbd8fa999caf381b0b
DmitryNuzhdin <mr.dmitrynuzhdin@yandex.ru>
2020-08-06 14:27:41 +0300

    Figure factory and figure type fix

--- src/main/java/task3/model/FigureFactory.java
+++ src/main/java/task3/model/FigureFactory.java
@@ -1,14 +1,23 @@
 package task3.model;
 
-import task3.model.figureTypes.Hero;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FigureFactory {
-    public Figure createFigure(FigureType figureType, int x, int y) //будем передавать тип и координату прорисовки
-    {
-       Figure figure;
-       switch (figureType){
-           case I:
-               figure = new Hero(new Coordinate(x,y));
-       }
+    public Figure createFigure(FigureType figureType) {//здесь достаточно передавать только тип, сдвинуть фигуру на нужную координату мы сегда можем и так
+        //фигуры друг от друга отличаются только набором элементов
+        Set<Coordinate> elements = new HashSet<>();
+        switch (figureType){
+            case I:
+                for (int i = 0; i < 4; i++) elements.add(new Coordinate(i, 0));
+                break;
+            case L:
+                for (int i = 0; i <= 3; i++) elements.add(new Coordinate(i, 1));
+                elements.add(new Coordinate(0, 0));
+                break;
+            default:
+                throw new RuntimeException("Figure type not supported");
+        }
+        return new Figure(0, 0, elements);
     }
 }
--- src/main/java/task3/model/FigureType.java
+++ src/main/java/task3/model/FigureType.java
@@ -1,40 +1,5 @@
 package task3.model;
 
-import java.util.Set;
-
 public enum FigureType {
     L, J, I, Z, S, T, O
-}
-
-class Hero(){
-    final int x;
-    final int y;
-    final Set<Coordinate> elements = null;
-
-    Hero (int x, int y) { //Соответствует I
-        this.x = x;
-        this.y = y;
-        int i = 1;
-        while (i < 5) {
-            this.elements.add(new Coordinate(x + i, y + i));
-            i++;
-        }
-    }
-
-    class Ricky () { //соответствует L
-        final int x;
-        final int y;
-        final Set<Coordinate> elements = null;
-
-        Ricky(int x, int y) { //Соответствует I
-            this.x = x;
-            this.y = y;
-            int i = 1;
-            while (i < 4) {
-                this.elements.add(new Coordinate(x + i, y + i));
-                i++;
-            }
-            this.elements.add(new Coordinate(x + i, y + i + 1));
-        }
-    }
 }

0f09d8448d33d427f48dcf885e4015e439bf71fb
Kasymkhankhubiev <kasymkhankhubievnis@gmail.com>
2020-08-31 17:24:19 +0700

    ServerSuccessAnswer Commit

--- src/main/java/task5/messages/ServerSuccessAnswer.java
+++ src/main/java/task5/messages/ServerSuccessAnswer.java
@@ -1,4 +1,25 @@
 package task5.messages;
 
+import java.util.Objects;
+
 public class ServerSuccessAnswer implements ServerMessage{
+    public final String message;
+
+    public ServerSuccessAnswer(String message){
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServerSuccessAnswer that = (ServerSuccessAnswer) o;
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
 }
